120dfefaae26fdc6a0ca51b39215568f37486e2e
techygrrrl <techygrrrl@proton.me>
2024-09-14 20:03:31 -0400

    scope appwrite script

--- scripts/sf6-rank-watchrrr.appwrite.user.js
+++ scripts/sf6-rank-watchrrr.appwrite.user.js
@@ -9,107 +9,113 @@
 // @require     https://cdn.jsdelivr.net/npm/appwrite@16.0.0
 // ==/UserScript==
 
-perform()
+(() => {
+  perform();
 
-async function perform() {
-  const tag = 'sf6-rank-watchrrr'
+  const tag = "sf6-rank-watchrrr";
 
-  const playerId = location.href.split('https://www.streetfighter.com/6/buckler/api/en/card/')[1]?.replace(/[^\d.-]+/g, '')
+  async function perform() {
+    const playerId = location.href
+      .split("https://www.streetfighter.com/6/buckler/api/en/card/")[1]
+      ?.replace(/[^\d.-]+/g, "");
 
-  const { databaseId, collectionId, projectId } = getConfig()
-  if (!databaseId || !collectionId || !projectId) {
-    throw new Error('Script misconfigured.')
-  }
+    const { databaseId, collectionId, projectId } = getConfig();
+    if (!databaseId || !collectionId || !projectId) {
+      throw new Error("Script misconfigured.");
+    }
 
-  const { Client, Databases } = Appwrite;
-  const client = new Client();
+    const { Client, Databases } = Appwrite;
+    const client = new Client();
 
-  client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(projectId)
+    client.setEndpoint("https://cloud.appwrite.io/v1").setProject(projectId);
 
-  const databases = new Databases(client)
+    const databases = new Databases(client);
 
-  const listResult = await databases.listDocuments(databaseId, collectionId, [])
-
-  const document = listResult.documents.find((doc) => doc.sid == playerId)
-
-  const playerData = getPlayerData()
-
-  try {
-    const recordResult = await databases[document ? 'updateDocument' : 'createDocument'](
+    const listResult = await databases.listDocuments(
       databaseId,
       collectionId,
-      playerId,
-      {
+      []
+    );
+
+    const document = listResult.documents.find((doc) => doc.sid == playerId);
+
+    const playerData = getPlayerData();
+
+    try {
+      const recordResult = await databases[
+        document ? "updateDocument" : "createDocument"
+      ](databaseId, collectionId, playerId, {
         sid: playerId,
-        data: JSON.stringify(playerData)
-      }
-    )
+        data: JSON.stringify(playerData),
+      });
 
-    console.log(tag, 'Successfully updated record', recordResult)
-    delayWindowRefresh()
-  } catch (err) {
-    console.error(tag, 'Failed to create or update record', err)
-  }
-}
-
-function getConfig() {
-
-  /////////////////
-  // Refresh time
-  /////////////////
-  const minRefreshTime = 60 // Refresh responsibly (minimum time is 1 refresh per minute)
-  const refreshTimeString = localStorage.getItem('refresh_time') || '120' // default: 2 mins
-  let refresh = parseInt(refreshTimeString)
-  if (isNaN(refresh)) {
-    refresh = 120
-  }
-  refresh = Math.max(minRefreshTime, refresh)
-
-  /////////////////
-  // AppWrite Project ID
-  /////////////////
-  const projectId = localStorage.getItem('appwrite_project_id')
-  if (!projectId) {
-    throw new Error('user script misconfigured. missing: appwrite_project_id ')
+      console.log(tag, "Successfully updated record", recordResult);
+      delayWindowRefresh();
+    } catch (err) {
+      console.error(tag, "Failed to create or update record", err);
+    }
   }
 
-  /////////////////
-  // AppWrite Database ID
-  /////////////////
-  const databaseId = localStorage.getItem('appwrite_database_id')
-  if (!databaseId) {
-    throw new Error('user script misconfigured. missing: appwrite_database_id ')
+  function getConfig() {
+    /////////////////
+    // Refresh time
+    /////////////////
+    const minRefreshTime = 60; // Refresh responsibly (minimum time is 1 refresh per minute)
+    const refreshTimeString = localStorage.getItem("refresh_time") || "120"; // default: 2 mins
+    let refresh = parseInt(refreshTimeString);
+    if (isNaN(refresh)) {
+      refresh = 120;
+    }
+    refresh = Math.max(minRefreshTime, refresh);
+
+    /////////////////
+    // AppWrite Project ID
+    /////////////////
+    const projectId = localStorage.getItem("appwrite_project_id");
+    if (!projectId) {
+      throw new Error(
+        "user script misconfigured. missing: appwrite_project_id "
+      );
+    }
+
+    /////////////////
+    // AppWrite Database ID
+    /////////////////
+    const databaseId = localStorage.getItem("appwrite_database_id");
+    if (!databaseId) {
+      throw new Error(
+        "user script misconfigured. missing: appwrite_database_id "
+      );
+    }
+
+    /////////////////
+    // AppWrite Collection ID
+    /////////////////
+    const collectionId = localStorage.getItem("appwrite_collection_id");
+    if (!collectionId) {
+      throw new Error(
+        "user script misconfigured. missing: appwrite_collection_id "
+      );
+    }
+
+    return {
+      refresh,
+      projectId,
+      databaseId,
+      collectionId,
+    };
   }
 
-
-  /////////////////
-  // AppWrite Collection ID
-  /////////////////
-  const collectionId = localStorage.getItem('appwrite_collection_id')
-  if (!collectionId) {
-    throw new Error('user script misconfigured. missing: appwrite_collection_id ')
+  function getPlayerData() {
+    return JSON.parse(document.body.innerText);
   }
 
-  return {
-    refresh,
-    projectId,
-    databaseId,
-    collectionId,
+  function delayWindowRefresh() {
+    const config = getConfig();
+
+    setTimeout(() => {
+      console.log(tag, "Refreshing...");
+      window.location.reload();
+    }, config.refresh * 1000);
   }
-}
-
-
-function getPlayerData() {
-  return JSON.parse(document.body.innerText)
-}
-
-function delayWindowRefresh() {
-  const config = getConfig()
-
-  setTimeout(() => {
-    console.log(tag, 'Refreshing...')
-    window.location.reload()
-  }, config.refresh * 1000)
-}
+})();
